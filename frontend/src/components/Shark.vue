3ec39cb430c8bbe3ed05dde478a94eaedb7e2b25
Link Li <nintendo.link@foxmail.com>
2025-01-03 14:42:39 +1100

    Communicate with backend

---
+++ frontend/src/components/Shark.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { ref, onMounted } from 'vue'
+import axios from 'axios'
+
+const msg = ref('')
+
+const getResponse = async () => {
+  try {
+    const path = 'http://localhost:8000/shark'
+    const res = await axios.get(path)
+    console.log(res.data)
+    msg.value = res.data
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+onMounted(() => {
+  getResponse()
+})
+</script>
+
+<template>
+  <div>
+    <button type="button" class="btn btn-primary">{{ msg }}</button>
+  </div>
+</template>
