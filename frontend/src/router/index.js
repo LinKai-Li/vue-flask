5b95a835c401bdac3538717efb2e9dca53de9683
Link Li <nintendo.link@foxmail.com>
2025-01-03 14:59:48 +1100

    Implement UI of games component

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Shark from '@/components/Shark.vue'
+import Games from '@/components/Games.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +9,11 @@ const router = createRouter({
       path: '/shark',
       name: 'Shark',
       component: Shark,
+    },
+    {
+      path: '/games',
+      name: 'Games',
+      component: Games,
     },
   ],
 })
