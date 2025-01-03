40bbac510f371b407c94679391a1dd634ef95f2e
Link Li <nintendo.link@foxmail.com>
2025-01-03 15:14:23 +1100

    Get Router for Game

--- backend/main.py
+++ backend/main.py
@@ -16,5 +16,41 @@ def greetings():
 def shark():
     return("Shark!")
 
+GAMES = [
+    {
+        'title': '2k21',
+        'genre': 'sports',
+        'played': True,
+    },
+    {
+        'title': 'Evil Within',
+        'genre': 'horror',
+        'played': False,
+    },
+    {
+        'title': 'the last of us',
+        'genre': 'survival',
+        'played': True,
+    },
+    {
+        'title': 'days gone',
+        'genre': 'horror/survival',
+        'played': False,
+    },
+    {
+        'title': 'mario',
+        'genre': 'retro',
+        'played': True,
+    },
+]
+
+# The GET route handler
+@app.route('/games', methods=['GET'])
+def all_games():
+    return jsonify({
+        'games': GAMES,
+        'status': 'success'
+    })
+
 if __name__ == '__main__':
     app.run(debug=True, port=8000)
