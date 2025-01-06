dd378769edee51069108f567e885d7b2f3a0d060
Link Li <nintendo.link@foxmail.com>
2025-01-06 14:38:12 +1100

    Add vue-toastification

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,6 +1,8 @@
 import "ant-design-vue/dist/reset.css";
 import "./style.css";
 import "../node_modules/flowbite-vue/dist/index.css";
+import Toast from "vue-toastification";
+import "vue-toastification/dist/index.css";
 
 import { createApp } from "vue";
 import store from "./store";
@@ -13,5 +15,6 @@ const app = createApp(App);
 app.use(router);
 app.use(Antd);
 app.use(store);
+app.use(Toast);
 
 app.mount("#app");
